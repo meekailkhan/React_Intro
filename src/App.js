71f5f95bfd0b9a4b3785a03c11c343f9f187d066
pc <meekailkhan123aar@gmail.com>
2025-01-26 14:05:43 +0530

    event handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
+import FunctionClick from './components/FunctionClick';
 // import Counter from './components/Counter';
-import Greet from './components/Greet';
+// import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
 // import Hello from './components/hello';
 // import Message from './components/Message';
@@ -10,16 +11,18 @@ function App() {
   return (
     <div className="App">
       {/* <Greet/> */}
-      <Greet firstName="meekail" lastName='aslam'/>
+      {/* <Greet firstName="meekail" lastName='aslam'/> */}
       {/* <Welcome name="meekail"/>
       <Welcome name="arbaaz"/>
       <Welcome name="mahir"/> */}
       {/* <Message/> */}
       {/* <Counter/> */}
-
       {/* <Hello name="meekail"><p>this is the child element</p></Hello>
       <Hello name="arbaaz"><button>this child btn</button></Hello>
       <Hello name="mahir"><pre>this is another child</pre></Hello> */}
+
+      <FunctionClick/>
+
 
     </div>
   );
